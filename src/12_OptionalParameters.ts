3f7b7d41b46dd8c716ce62f303ae90d42161c752
aleksandarspasovv <777alexander.spasov@gmail.com>
2026-03-24 05:44:48 +0200

    add: 12

--- src/12_OptionalParameters.ts
+++ src/12_OptionalParameters.ts
@@ -1,4 +1,15 @@
-function printNameAndAge(name: string, age?:number){
-    console.log(name, age)
+type Options = {
+    debugMode?: boolean,
+    indentLevel?: number,
 }
 
+function printName(name: string, {debugMode = false, indentLevel}: Options = {}){
+    console.log(name, debugMode, indentLevel)
+}
+
+printName('Alex')
+
+
+function sum(...nums: number[]){
+    return //...
+}
