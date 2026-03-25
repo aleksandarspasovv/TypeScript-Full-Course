6a7faa84502022e705843908e9ee80eeb887e140
aleksandarspasovv <777alexander.spasov@gmail.com>
2026-03-25 14:09:49 +0200

    add: 22

---
+++ src/22_asConstAndEnums.ts
@@ -0,0 +1,22 @@
+let a = 1 as const //makes let == const
+
+let nums = [1, 2, 3] as const //hardcodes nums[0] to 1
+
+const SKILL_LEVELS = ['Beginner', 'Intermediate', 'Expert'] as const
+
+type Person = {
+    skilLevel: (typeof SKILL_LEVELS)[number]
+}
+
+SKILL_LEVELS.forEach(skill => {
+    console.log(skill)
+})
+
+const bestPerson = {
+    name: 'Maggie',
+    age: 26,
+    address: {
+        planet: 'Saturn'
+    }
+}
+
